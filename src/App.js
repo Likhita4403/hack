975e8266288b194c54102a0a812413e6bc6a0f44
Sai Preeti <psaipreeti@gmail.com>
2022-06-24 15:06:37 +0530

    Appbar with routing and login page created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Login from './components/Login/Login'
+import DrawerAppBar from './components/AppBar/AppBar'
+import {Route,Routes} from 'react-router-dom'
+<Routes>
+  <Route path='/Home' element={<Home/>}/>
+  <Route path='/Contact' element={<Contact/>}/>
+  <Route path='/Login' element={<Login/>}/>
+  <Route path='/About' element={<About/>}/>
+</Routes>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <div>
+          <DrawerAppBar/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/firebase_connect.js
@@ -0,0 +1,16 @@
+// import { initializeApp } from "firebase/app";
+// import { getFirestore } from "firebase/firestore";
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDFh8BlfdeKH4hByG1argaSugxSM4aMs3k",
+//   authDomain: "authentication-9efe9.firebaseapp.com",
+//   projectId: "authentication-9efe9",
+//   storageBucket: "authentication-9efe9.appspot.com",
+//   messagingSenderId: "968031093599",
+//   appId: "1:968031093599:web:6c7e8d0342c55a9cc36cea",
+//   measurementId: "G-MDV0E3R66X"
+// };
+
+// const app = initializeApp(firebaseConfig);
+// export const db= getFirestore(app);
+
